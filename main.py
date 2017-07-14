d9dca9a8dbf8304e62eb211532740135c6f577b3
Joaquin Sargiotto <joaquinsargiotto@gmail.com>
2017-07-13 23:39:23 -0300

    Rename add_metric to avoid

--- main.py
+++ main.py
@@ -47,38 +47,49 @@ def async_call(function, *args, **kwargs):
     return task
 
 
+def ns_to_sec(value):
+    """
+    Converts a value in nanoseconds to seconds.
+    """
+    return value / 1000000000.0
+
+
 class BaseStatsCollector(object):
 
     def __init__(self):
-        self._metrics = {}
+        self._stats = {}
 
-    def add_metric(self, metric_id, metric_type, metric_desc=None, labels=None):
-        self._metrics[metric_id] = metric_type(metric_id, metric_desc, labels=labels)
+    def add_stat(self, stat_id, stat_type, stat_desc=None, labels=None):
+        self._stats[stat_id] = stat_type(stat_id, stat_desc, labels=labels)
 
-    def get_metric(self, metric_id):
-        return self._metrics[metric_id]
+    def get_stat(self, stat_id):
+        return self._stats[stat_id]
 
-    def get_metrics(self):
-        return self._metrics.values()
+    def get_stats(self):
+        return self._stats.values()
 
     def add_gauge(self, gauge_id, gauge_desc=None, labels=None):
-        return self.add_metric(gauge_id, prometheus.GaugeMetricFamily,
-                               gauge_desc, labels=labels)
+        return self.add_stat(gauge_id, prometheus.GaugeMetricFamily,
+                             gauge_desc, labels=labels)
 
     def add_counter(self, counter_id, counter_desc=None, labels=None):
-        return self.add_metric(counter_id, prometheus.CounterMetricFamily,
-                               counter_desc, labels=labels)
+        return self.add_stat(counter_id, prometheus.CounterMetricFamily,
+                             counter_desc, labels=labels)
 
     def cleanup_samples(self):
-        for metric in self.get_metrics():
-            metric.samples = []
+        for stat in self.get_stats():
+            stat.samples = []
+
+    def collect(self):
+        """Return the stats"""
+        return self._stats.values()
 
 
 class NetworkStatsCollector(BaseStatsCollector):
 
     # Which metric names are associated with each
     # of the fields in the stat json
-    METRIC_NAME_TO_STAT = {
+    METRIC_NAME_TO_DOCKER_STAT = {
         "container_network_receive_bytes_total": "rx_bytes",
         "container_network_receive_errors_total": "rx_errors",
         "container_network_receive_packets_total": "rx_packets",
@@ -90,7 +101,7 @@ class NetworkStatsCollector(BaseStatsCollector):
     }
 
     def __init__(self):
-        super(NetworkStatsCollector, self).__init__()
+        super().__init__()
         labels = ["interface", "appid", "taskid"]
         self.add_counter("container_network_receive_bytes_total",
                          "Cumulative count of bytes received", labels)
@@ -112,26 +123,23 @@ class NetworkStatsCollector(BaseStatsCollector):
     def add_container(self, appid, taskid, container_stats):
         nets = container_stats["networks"]
         for net in nets:
-            for metric_name in self.METRIC_NAME_TO_STAT:
-                stat_id = self.METRIC_NAME_TO_STAT[metric_name]
-                self.get_metric(metric_name).add_metric([net, appid, taskid], nets[net][stat_id])
-
-    def collect(self):
-        return self._metrics.values()
+            for metric_name in self.METRIC_NAME_TO_DOCKER_STAT:
+                stat_id = self.METRIC_NAME_TO_DOCKER_STAT[metric_name]
+                self.get_stat(metric_name).add_metric([net, appid, taskid], nets[net][stat_id])
 
 
 class MemoryStatsCollector(BaseStatsCollector):
 
     # Which metric names are associated with each
     # of the fields in the stat json
-    METRIC_NAME_TO_STAT = {
+    METRIC_NAME_TO_DOCKER_STAT = {
         "container_memory_cache": "cache",
         "container_memory_rss": "rss",
         "container_memory_swap": "swap",
     }
 
     def __init__(self):
-        super(MemoryStatsCollector, self).__init__()
+        super().__init__()
         labels = ["appid", "taskid"]
         self.add_gauge("container_memory_cache", "Number of bytes of page cache memory.", labels)
         self.add_gauge("container_memory_rss", "Size of RSS in bytes.", labels)
@@ -145,24 +153,20 @@ class MemoryStatsCollector(BaseStatsCollector):
 
     def add_container(self, appid, taskid, container_stats):
         mem = container_stats["memory_stats"]
-        for metric_name in self.METRIC_NAME_TO_STAT:
-            stat_id = self.METRIC_NAME_TO_STAT[metric_name]
-            self.get_metric(metric_name).add_metric([appid, taskid], mem["stats"][stat_id])
+        for metric_name in self.METRIC_NAME_TO_DOCKER_STAT:
+            stat_id = self.METRIC_NAME_TO_DOCKER_STAT[metric_name]
+            self.get_stat(metric_name).add_metric([appid, taskid], mem["stats"][stat_id])
         # memory_usage needs special treatment
-        self.get_metric("container_memory_usage_bytes").add_metric([appid, taskid], mem["usage"])
+        self.get_stat("container_memory_usage_bytes").add_metric([appid, taskid], mem["usage"])
 
         mem_percent = (mem["usage"] / mem["limit"]) * 100
-        self.get_metric("container_memory_usage_percent").add_metric([appid, taskid], mem_percent)
-
-
-    def collect(self):
-        return self._metrics.values()
+        self.get_stat("container_memory_usage_percent").add_metric([appid, taskid], mem_percent)
 
 
 class CPUStatsCollector(BaseStatsCollector):
 
     def __init__(self):
-        super(CPUStatsCollector, self).__init__()
+        super().__init__()
         labels = ["appid", "taskid"]
         self.add_counter("container_cpu_system_seconds_total",
                          "Cumulative system cpu time consumed in seconds.", labels)
@@ -182,35 +186,35 @@ class CPUStatsCollector(BaseStatsCollector):
         cpu_usage = cpu_stats["cpu_usage"]
 
         # system, kernel and user usage
-        self.get_metric("container_cpu_system_seconds_total").add_metric([appid, taskid],
-                        cpu_stats["system_cpu_usage"] / 1000000000)
-        self.get_metric("container_cpu_kernel_seconds_total").add_metric([appid, taskid],
-                        cpu_usage["usage_in_kernelmode"] / 1000000000)
-        self.get_metric("container_cpu_user_seconds_total").add_metric([appid, taskid],
-                        cpu_usage["usage_in_usermode"] / 1000000000)
+        self.get_stat("container_cpu_system_seconds_total").add_metric(
+            [appid, taskid],
+            ns_to_sec(cpu_stats["system_cpu_usage"]))
+        self.get_stat("container_cpu_kernel_seconds_total").add_metric(
+            [appid, taskid],
+            ns_to_sec(cpu_usage["usage_in_kernelmode"]))
+        self.get_stat("container_cpu_user_seconds_total").add_metric(
+            [appid, taskid],
+            ns_to_sec(cpu_usage["usage_in_usermode"]))
 
         # Per cpu metrics
         for cpu, value in enumerate(cpu_usage["percpu_usage"]):
             cpu_label = "cpu{:02d}".format(cpu)
-            self.get_metric("container_cpu_usage_seconds_total").add_metric(
+            self.get_stat("container_cpu_usage_seconds_total").add_metric(
                 [cpu_label, appid, taskid],
-                value / 1000000000)
+                ns_to_sec(value))
 
         # Calculate percent usage
         # https://github.com/moby/moby/blob/8a03eb0b6cc56879eada4a928c6314f33001fc83/integration-cli/docker_api_stats_test.go#L40
         cpu_delta = cpu_usage["total_usage"] - pre_cpu_stats["cpu_usage"]["total_usage"]
         sys_delta = cpu_stats["system_cpu_usage"] - pre_cpu_stats["system_cpu_usage"]
         cpu_percent = (cpu_delta / sys_delta) * len(cpu_usage["percpu_usage"]) * 100.0
-        self.get_metric("container_cpu_usage_percent").add_metric([appid, taskid], cpu_percent)
-
-    def collect(self):
-        return self._metrics.values()
+        self.get_stat("container_cpu_usage_percent").add_metric([appid, taskid], cpu_percent)
 
 
 class LRUCacheStatsCollector(BaseStatsCollector):
 
     def __init__(self, cached_method):
-        super(LRUCacheStatsCollector, self).__init__()
+        super().__init__()
         self.add_counter("exporter_details_cache_hits_total", "Cumulative cache hits.")
         self.add_counter("exporter_details_cache_misses_total", "Cumulative cache misses.")
         self.add_gauge("exporter_details_cache_max_size", "Maximum size of the cache.")
@@ -218,18 +222,16 @@ class LRUCacheStatsCollector(BaseStatsCollector):
 
         self._cached_method = cached_method
 
-    def add_container(self, appid, taskid, stats):
-        pass
-
     def collect(self):
+        # Get cache info
         info = self._cached_method.cache_info()
 
-        self.get_metric("exporter_details_cache_hits_total").add_metric([], info.hits)
-        self.get_metric("exporter_details_cache_misses_total").add_metric([], info.misses)
-        self.get_metric("exporter_details_cache_max_size").add_metric([], info.maxsize)
-        self.get_metric("exporter_details_cache_current_size").add_metric([], info.currsize)
+        self.get_stat("exporter_details_cache_hits_total").add_metric([], info.hits)
+        self.get_stat("exporter_details_cache_misses_total").add_metric([], info.misses)
+        self.get_stat("exporter_details_cache_max_size").add_metric([], info.maxsize)
+        self.get_stat("exporter_details_cache_current_size").add_metric([], info.currsize)
 
-        return self._metrics.values()
+        return super().collect()
 
 
 class DockerStatsCollector(object):
@@ -239,8 +241,7 @@ class DockerStatsCollector(object):
         self._subcollectors = [
             NetworkStatsCollector(),
             MemoryStatsCollector(),
-            CPUStatsCollector(),
-            LRUCacheStatsCollector(DockerStatsCollector._cached_details)
+            CPUStatsCollector()
         ]
         self._lock = threading.Lock()
 
@@ -280,20 +281,27 @@ class DockerStatsCollector(object):
         self.add_container(appid, taskid, stats)
 
     @lru_cache(maxsize=32)
-    def _cached_details(self, container_id):
+    def _details(self, container_id):
         """
         Return the details for `container_id`
         """
         return self._client.api.inspect_container(container_id)
 
+    def cache_info(self):
+        """
+        Returns the cache info.
+        """
+        return self._details.cache_info()
+
     def add_container(self, appid, taskid, stats):
         with self._lock:
-            for metrics in self._subcollectors:
-                metrics.add_container(appid, taskid, stats)
+            for collector in self._subcollectors:
+                collector.add_container(appid, taskid, stats)
 
     def cleanup_samples(self):
-        for metrics in self._subcollectors:
-            metrics.cleanup_samples()
+        with self._lock:
+            for collector in self._subcollectors:
+                collector.cleanup_samples()
 
     def collect(self):
         """
@@ -308,7 +316,7 @@ class DockerStatsCollector(object):
         for container in self._client.api.containers(all=False):
             # Get the details of the container (cached for speed)
             cid = container["Id"]
-            details = self._cached_details(cid)
+            details = self._details(cid)
 
             # Get appid and taskid from environment configuration
             appid = taskid = None
@@ -348,15 +356,19 @@ def main():
                       help="Path under which to expose metrics.")
     args = argp.parse_args()
 
-    # Create collector
-    collector = DockerStatsCollector(host=os.environ.get("DOCKER_REMOTE_HOST", "127.0.0.1"),
-                                     port=os.environ.get("DOCKER_REMOTE_PORT", "2376"),
-                                     client_cert=os.environ.get("DOCKER_SSL_CLIENT_CERT"),
-                                     client_key=os.environ.get("DOCKER_SSL_CLIENT_KEY"),
-                                     ca_cert=os.environ.get("DOCKER_SSL_CA_CERT"))
+    # Register docker stats collector
+    docker_collector_params = {
+        "host": os.environ.get("DOCKER_REMOTE_HOST", "127.0.0.1"),
+        "port": os.environ.get("DOCKER_REMOTE_PORT", "2376"),
+        "client_cert": os.environ.get("DOCKER_SSL_CLIENT_CER,T"),
+        "client_key": os.environ.get("DOCKER_SSL_CLIENT_KEY",),
+        "ca_cert": os.environ.get("DOCKER_SSL_CA_CERT"),
+    }
+    prometheus.REGISTRY.register(DockerStatsCollector(**docker_collector_params))
 
-    # Register our collector
-    prometheus.REGISTRY.register(collector)
+    # Register cache stats collector
+    prometheus.REGISTRY.register(LRUCacheStatsCollector(DockerStatsCollector))
+
     # Remove process collector (added by default)
     prometheus.REGISTRY.unregister(PROCESS_COLLECTOR)
 
